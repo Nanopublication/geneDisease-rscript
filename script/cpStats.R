2ea219b3c9029d2d172e8dbdbeea84dfd516c579
rr.kaliyaperumal@gmail.com <whoisnext5>
2014-09-10 10:04:40 +0200

    scatterPlot and improved cpStats

--- script/cpStats.R
+++ script/cpStats.R
@@ -7,19 +7,34 @@
 @since 03-09-2014
 
 " 
+library(lattice)
 # Read csv file
-mydf <- read.csv('/home/rajaram/eelke_pc/results/analysis2/geneConceptProfileStats', header= T, fileEncoding= "windows-1252")
+mydf <- read.csv('/home/rajaram/eelke_pc/Meuk/cpgp/results/analysis2/geneConceptProfileStats', 
+                 colClasses=c("integer", "integer", "integer", "integer", "integer" ), header= T, fileEncoding= "windows-1252")
 #Plot parameters
 xLimit <- c(0, 10000)
 dataSize <- c(500)
 title <- c("diseasesConceptProfileStats")
 
 # Get data from 'conceptProfileLength' column
-data = mydf$conceptProfileLength
+conceptProfileLength <- mydf$conceptProfileLength
+# Get data from 'noOfPMids' column
+noOfAbstracts <- mydf$noOfPMids
 # Find maximum profile length
-maxLength <- max(data)
+maxLength <- max(conceptProfileLength)
 # Find profles median value
-median_ <- median(data)
+median_ <- median(conceptProfileLength)
 
-hist(data, prob=TRUE, xlim= xLimit,   col="black", breaks=dataSize, main="diseasesConceptProfileStats") # prob=TRUE for probabilities not counts
+hist(conceptProfileLength, prob=TRUE, xlim= xLimit,   col="black", breaks=dataSize, main="overlappingConceptProfileStats") # prob=TRUE for probabilities not counts
 
+plot(noOfAbstracts, conceptProfileLength, type="l", main="Fig 2c") 
+
+xyplot(conceptProfileLength ~ noOfAbstracts, grid = TRUE, type = c("p"))
+
+#  xyplot(log10(conceptProfileLength) ~ log10(noOfAbstracts), grid = TRUE,  
+#         type = c("p", "smooth"), col.line = "darkorange", lwd = 3)
+
+# xyplot(conceptProfileLength ~ noOfAbstracts,
+#        grid = TRUE,
+#        scales = list(x = list(log = 10, equispaced.log = FALSE)),
+#        type = c("p", "smooth"), col.line = "darkorange", lwd = 3)
---
+++ script/supplementalMethodsScatterPlot.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(reshape)
+
+genedf <- read.csv('/home/rajaram/mysql-data/matchscores_no_NaN_grt_01_no_grt_1.txt', colClasses=c("integer", "integer", "double", "integer", "integer"), header= T, fileEncoding= "windows-1252")
+
+
+x <- (genedf$geneAbstract)
+y <- (genedf$diseaseAbstract)
+z <- log10(genedf$score)
+
+#x <- (genedf$geneAbstract)
+#y <- (genedf$diseaseAbstract)
+#z <- (genedf$score)
+
+xLabel <- c("No. of abstracts for gene")
+yLabel <- c("No. of abstracts for disease")
+pointWidth <- c(0.1)
+markerStyle <- c(19)
+xLimit <- c(0, 20000)
+yLimit <- c(0, 40000)
+
+#png("/tmp/myplot.png", width=16, height=16, units="in", res=300)
+
+
+
+d <- data.frame(x, y, z)
+dg<-qplot(x, y, colour = z, data = d, xlab = xLabel, ylab = yLabel)
+#dg<-qplot(x, y, colour = z, data = d, xlab = xLabel, ylab = yLabel, xlim = xLimit, ylim = yLimit)
+dg + scale_colour_gradient(low="blue", high="red",  name="Match score\n(As numeric)")
+
+#dev.off() #only 129kb in size
